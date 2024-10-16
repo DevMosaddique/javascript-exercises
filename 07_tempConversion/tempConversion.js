f2ffc3cbf606ab4456ffcf98b69b83ad5ca42e0a
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-10-16 19:52:07 +0000

    Learned about ParseFloat/Int && toFixed()
    toFixed returns a string so we used parse... for converting it to number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let tempInCelsius = (temp - 32) * (5 / 9)
+    return parseFloat(tempInCelsius.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let tempInFahrenheit = (temp * (9/5)) + 32
+    return Math.round(tempInFahrenheit * 10) / 10
 };
 
+// 100 && -100
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
