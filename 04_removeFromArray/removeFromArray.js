63166f7b579b0689b5193f10503ff9d4453824bc
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-10-16 11:19:32 +0000

    ...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...args) {
+// const newArray = []
+// arr.forEach(num => {
+// if (!args.includes(num)) {
+//     newArray.push(num)
+// }
+// });
+// return newArray
+return arr.filter(num => !args.includes(num))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
