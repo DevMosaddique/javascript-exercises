df47a06506ace03058444a65ade4b71becf0817c
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-10-16 12:26:17 +0000

    updated for effeciency

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,17 @@
 const sumAll = function(startNum, endNum) {
 
+    if(startNum < 0 || endNum < 0) return 'ERROR'
+    if(!Number.isInteger(startNum) || !Number.isInteger(endNum)) return 'ERROR'
+    if(startNum > endNum) {
+        [startNum, endNum] = [endNum, startNum]
+    }
+
     let sum = 0
-    if(startNum < 0 || endNum < 0){
-         return 'ERROR'
-    }else if(parseInt(startNum) !== startNum || parseInt(endNum) !== endNum){
-        return 'ERROR'
-    }else if(isNaN(startNum) || isNaN(endNum)){
-        return 'ERROR'
-    }else if(startNum > endNum){
-        for(let i = startNum; i >= endNum; i--){
-            sum += i
-        }
-    }else{
-        for(let i = startNum; i<=endNum; i++){
-            sum += i
-        }
+    for(let i = startNum; i<=endNum; i++){
+        sum += i
     }
     return sum
-    // console.log(sum);
 };
-// sumAll(2.5, 1)
 
 // Do not edit below this line
 module.exports = sumAll;
