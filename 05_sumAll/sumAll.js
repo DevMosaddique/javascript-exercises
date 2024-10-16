901ad97d2d0a7356decc8a57a3ee9e62962d57b6
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-10-16 12:06:50 +0000

    Added all of them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    let sum = 0
+    if(startNum < 0 || endNum < 0){
+         return 'ERROR'
+    }else if(parseInt(startNum) !== startNum || parseInt(endNum) !== endNum){
+        return 'ERROR'
+    }else if(isNaN(startNum) || isNaN(endNum)){
+        return 'ERROR'
+    }else if(startNum > endNum){
+        for(let i = startNum; i >= endNum; i--){
+            sum += i
+        }
+    }else{
+        for(let i = startNum; i<=endNum; i++){
+            sum += i
+        }
+    }
+    return sum
+    // console.log(sum);
 };
+// sumAll(2.5, 1)
 
 // Do not edit below this line
 module.exports = sumAll;
