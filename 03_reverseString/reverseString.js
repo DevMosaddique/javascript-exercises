b8cc9a6bc69fea984ccbccc5816068be8d5a7456
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-10-16 05:30:51 +0000

    Reverse sreing done
    usecase of Array(fron) method, reverse, join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  let newArr = word.split("");
+  newArr.reverse();
 
+  let stringA = newArr.join("");
+  return stringA;
+  
+  /* Easy-peasy way
+  ++++++++++++++++++++
+  return word.split('').reverse().join('') 
+  return Array.from(word).reverse().join('') 
+  
+  */
 };
+
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
