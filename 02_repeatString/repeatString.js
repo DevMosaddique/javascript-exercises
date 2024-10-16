c4e159f479afad8ce144a17369d3ffc3085ef09f
Hazrat Mosaddique Ali <mosaddique.dev@gmail.com>
2024-10-16 04:48:27 +0000

    repeat string  done
    at first i forgot about the return , that's why i got error many times at stsart . lol

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let newString = "";
+    for (let index = 0; index < num; index++) {
+      newString += string;
+    }
 
+    return newS;
+  }
 };
+
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
